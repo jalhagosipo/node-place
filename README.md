d68a012df9b51a8abb2103f007b5ee255b42a10a
jalhagosipo <gpfla757@naver.com>
2020-02-18 23:49:35 +0900

    chore: add views

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # node-place
-- 구글 지도/장소 API 를 사용해 검색한 장소의 위치를 보여주고 즐겨찾기로 관리하기
+- ~~구글 지도/장소 API~~ 카카오/네이버 API 를 사용해 검색한 장소의 위치를 보여주고 즐겨찾기로 관리하기
 
 ## v0.1
 - 프로젝트 설정
-    - 프론트 : Google Maps API 사용. 장소표시 , 백엔드 : Google Places API 사용. 장소검색
+    - 프론트 : ~~ Google Maps API ~~ KAKAO MAP API 사용. 장소표시 , 백엔드 : ~~ Google Places API ~~ NAVER API 사용. 장소검색
     - 검색을 통해 위치를 파악하여 위치 데이터를 프론트로 보냄
     - 딱히 관계형 디비가 필요하지 않으므로 몽고디비사용
         - favorite.js : 즐겨찾기
@@ -12,3 +12,30 @@
         - COOKIE_SECRET=nodeplacesecret
         - MONGO_ID=아이디
         - MONGO_PASSWORD=비밀번호
+
+## v0.2
+- NAVER API를 사용하기 위해 키 발급 (NAVER API중 Geocoding을 사용)
+    - .env에 `PLACES_API_KEY=키` 추가
+- 간단히 검색 보여주기
+    - `/`, `/autocomplete/:query`, `/search/:query` 라우터 추가
+    - views 추가
+
+## v0.3
+- 프론트: 화면에 지도보여주기
+
+## v0.4
+- 내 위치 주변을 검색하는 API와 특정 종류의 장소(카페, 병원 등)만 검색하는 API
+- 즐겨찾기 추가버튼 클릭하면 /로 가도록
+
+## v1.0
+- /에서 즐켜찾기 위치가 나오도록
+
+## v1.1
+- 즐겨찾기 삭제하기
+    - 라우터 생성 및 프론트에서 삭제버튼 추가
+
+## v1.2
+- 검색 내역 표시하기
+    - 검색내역을 불러와 화면에 랜더링
+    
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const axios = require('axios');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const History = require('../schemas/history');
+
+const router = express.Router();
+
+// 메인 화면을 보여주는 라우터
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
+// 검색어 자동완성 라우터
+router.get('/autocomplete/:query', (req, res, next) => {
+
+});
+
+// 실제 장소 검색 시 결과를 반환
+router.get('/search/:query', async (req, res, next) => {
+
+  try {
+    // 검색내역을 구현하기 위해 검색어 저장
+    const history = new History({ query: req.params.query });
+    await history.save();
+
+    const params = { 'query' : encodeURI(req.params.query) };
+    const headers = { 'X-Naver-Client-Id':process.env.NAVER_GEOCODE_API_CLIENT_ID, 'X-Naver-Client-Secret': process.env.NAVER_GEOCODE_API_CLIENT_KEY };
+
+   const result = await axios.get('https://openapi.naver.com/v1/map/geocode', {params, headers})
+      .then(response => {
+        console.log(response);
+        console.log(response.data.representation);
+      }).catch((error) => {
+        console.error(error);
+      });
+      console.log(result);
+
+      res.render('result', {
+        title: `${req.params.query} 검색 결과`,
+        results: result || '없음',
+        query: req.params.query,
+      });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 module.exports = router;
