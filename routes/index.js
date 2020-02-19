e22e0c320b9fc32bf9d233b93d7ac42a584811f1
jalhagosipo <gpfla757@naver.com>
2020-02-19 20:37:19 +0900

    chore: change API URI

--- routes/index.js
+++ routes/index.js
@@ -24,22 +24,20 @@ router.get('/search/:query', async (req, res, next) => {
     await history.save();
 
     const params = { 'query' : encodeURI(req.params.query) };
-    const headers = { 'X-Naver-Client-Id':process.env.NAVER_GEOCODE_API_CLIENT_ID, 'X-Naver-Client-Secret': process.env.NAVER_GEOCODE_API_CLIENT_KEY };
+    const headers = { 'X-NCP-APIGW-API-KEY-ID':process.env.NAVER_GEOCODE_API_CLIENT_ID, 'X-NCP-APIGW-API-KEY': process.env.NAVER_GEOCODE_API_CLIENT_KEY };
 
-   const result = await axios.get('https://openapi.naver.com/v1/map/geocode', {params, headers})
+    await axios.get('https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode', {params, headers})
       .then(response => {
-        console.log(response);
-        console.log(response.data.representation);
+        console.log(response.data);
+        res.render('result', {
+          title: `${req.params.query} 검색 결과`,
+          results: response.data || '없음',
+          query: req.params.query,
+        });
       }).catch((error) => {
         console.error(error);
       });
-      console.log(result);
-
-      res.render('result', {
-        title: `${req.params.query} 검색 결과`,
-        results: result || '없음',
-        query: req.params.query,
-      });
+      
   } catch (error) {
     console.error(error);
     next(error);
